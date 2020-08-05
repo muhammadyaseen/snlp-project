c799409c94fa994c88511d47c3c66ac2b4c5c574
Muhammad Yaseen <mohammad_yaseen13@live.com>
2020-08-05 19:45:16 +0200

    added answer patterns

--- code/baseline.py
+++ code/baseline.py
@@ -7,18 +7,27 @@ trec_corpus_xml = data_root + "trec_documents.xml"
 
 question_extraction_pattern = "Number: (\d+) *\n\n\<desc\> Description\:\n(\w+.*)\n\n\<\/top>"
 
-def get_test_questions(test_questions):
+def get_test_questions(test_questions, ans_patterns):
     
+    # get question id and text
+    qs = {}
     questions_doc = open(test_questions).read()
-
     question_extraction_pattern = "^\<num\> Number: (\d+) *\n\n\<desc\> Description\:\n(\w+.*)\n\n\<\/top>$"
     result = re.findall(question_extraction_pattern, questions_doc, re.MULTILINE)
 
-    for r in result:
-        print (r[0], " : " + r[1])
+    for q in result:
+        qs[int(q[0])] = {'question': q[1], 'ans_patterns': []}
 
-def get_answer_patterns(ans_patterns):
-    pass
+    # get associated answer patterns
+    ans_doc = open(ans_patterns).readlines()
+
+    for ap in ans_doc:
+        #print(ap)
+        ap = ap.split(" ")
+        id, pattern = ap[0], " ".join(ap[1:])
+        qs[int(id)]['ans_patterns'].append(pattern) 
+
+    return qs
 
 def process_trec_xml(trec_corpus_xml):
     pass
@@ -28,6 +37,8 @@ def process_trec_xml(trec_corpus_xml):
 
 if __name__ == "__main__":
     
-    get_test_questions(test_questions)
+    parsed = get_test_questions(test_questions, ans_patterns)
+
+    for p in parsed.values(): print(p)
 
     print("Oh boi! I'm up!")
